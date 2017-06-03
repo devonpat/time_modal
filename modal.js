59ac0c61ed90574491d3acc2ba01acb603defd47
Devon Patey <devon.patey@gmail.com>
2017-06-02 22:02:56 -0600

    created layout

--- modal.js
+++ modal.js
@@ -1,9 +1,9 @@
-/* Set the width of the side navigation to 250px */
+// width of side nav when open
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 }
 
-/* Set the width of the side navigation to 0 */
+// width of side nav when closed
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
