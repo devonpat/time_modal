185f2f1c22dee3534be3f1d1c62b7c898f698e1d
Devon Patey <devon.patey@gmail.com>
2017-06-02 23:22:53 -0600

    added model

--- modal.js
+++ modal.js
@@ -7,3 +7,29 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+var modal = document.getElementById('modal');
+
+var span = document.getElementsByClassName("close")[0];
+
+// close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+setTimeout(function (){
+	modal.style.display = "block";
+}, 3000);
+
+//submit button in Modal
+let submit = document.getElementById('submit');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+submit.addEventListener('click', func);
+function func () {
+	if (email.value.length > 6 && password.value.length > 4) {
+		alert("Thank you, an email will be sent shortly.")
+	}else {
+		alert('Invalid email/password')
+	}
+};
